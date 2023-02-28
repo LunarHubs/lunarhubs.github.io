0e4165aa90f6ee793eaedcb35093001c82ccb44b
LunarHubs <120012396+TrixzyLol@users.noreply.github.com>
2023-02-28 12:43:42 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+const scrollToTopButton = document.querySelector(".scroll-to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    scrollToTopButton.classList.add("show");
+  } else {
+    scrollToTopButton.classList.remove("show");
+  }
+});
+
+scrollToTopButton.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
